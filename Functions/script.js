86bdc06463449b98275b60161c4d546e110f9833
Ivan Epou <ivanepou92@gmail.com>
2022-10-22 12:21:28 +0300

    Functions

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,29 @@
-console.log('Testing')
+//function Declaration
+//Function can be called before is declared (Hoisting)
+//Hoisting - Is the process of moving function declarations to be the top of the file -
+//this only applies to functions with the function declaration syntax.
+function walk() {
+    console.log('Walk');
+}
+
+//function expressions can not be called before it's declaration.
+//Anonymous function Expressions
+let run = function () {
+    console.log('Run');
+}
+
+//Named function Expression
+let run2 = function run1() {
+    console.log('Run2')
+}
+
+//using arguments
+
+function sum() {
+    let total = 0;
+    for (let value of arguments)
+        total+=value;
+    console.log(total)
+}
+
+sum(1, 2, 5, 3);
